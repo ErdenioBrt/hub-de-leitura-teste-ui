6220758a9619072e96d54eadf75c517351963dbb
Erdenio Brito <erdeniobrt@gmail.com>
2026-08-24 17:52:45 -0300

    Implementação de testes

---
+++ cypress/e2e/cadastro.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
+
+describe("Funcionalide: Cadastro de livros", () => {
+    beforeEach(() => {
+        cy.visit('catalog.html');
+    
+    });
+    it('Deve clicar no botão adicionar à cesta', () => {
+        
+    });
+});
