eb1b87577f87b19e15d45a35aa512ac59a21535b
Erdenio Brito <erdeniobrt@gmail.com>
2026-08-25 09:58:31 -0300

    Testes usando faker

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
+import { faker } from "@faker-js/faker";
 
 describe("Funcionalide: Cadastro no Hub de Leitura", () => {
 
     beforeEach(() => {
         cy.visit('register.html');
-    
+
     });
 
-    it('Deve realizar o cadastro com sucesso', () => {
+    it('Deve realizar o cadastro com sucesso, usando função JS', () => {
         let email = `teste${Date.now()}@teste.com`
         cy.get('#name').type('Maria')
         cy.get('#email').type(email)
@@ -20,4 +21,18 @@ describe("Funcionalide: Cadastro no Hub de Leitura", () => {
         cy.url().should('include', 'dashboard')
     });
 
+    it('Deve fazer cadastro com sucesso, usando faker', () => {
+        let nome = faker.person.fullName()
+        let email = faker.internet.email()
+        cy.get('#name').type(nome)
+        cy.get('#email').type(email)
+        cy.get('#phone').type('213890123')
+        cy.get('#password').type('senha123')
+        cy.get('#confirm-password').type('senha123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        //resultado esperado do registro
+        cy.url().should('include', 'dashboard')
+        cy.get('#user-name').should('contain', nome)
+    });
 });
