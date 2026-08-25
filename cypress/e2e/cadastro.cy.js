dd216569a1bb686d73d531e1be874189b0ecb0ea
Erdenio Brito <erdeniobrt@gmail.com>
2026-08-25 09:46:05 -0300

    Teste de cadastro

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,12 +1,23 @@
 /// <reference types="cypress" />
-import { faker } from "@faker-js/faker";
 
-describe("Funcionalide: Cadastro de livros", () => {
+describe("Funcionalide: Cadastro no Hub de Leitura", () => {
+
     beforeEach(() => {
-        cy.visit('catalog.html');
+        cy.visit('register.html');
     
     });
-    it('Deve clicar no botão adicionar à cesta', () => {
-        
+
+    it('Deve realizar o cadastro com sucesso', () => {
+        let email = `teste${Date.now()}@teste.com`
+        cy.get('#name').type('Maria')
+        cy.get('#email').type(email)
+        cy.get('#phone').type('213890123')
+        cy.get('#password').type('senha123')
+        cy.get('#confirm-password').type('senha123')
+        cy.get('#terms-agreement').check()
+        cy.get('#register-btn').click()
+        //resultado esperado do registro
+        cy.url().should('include', 'dashboard')
     });
+
 });
